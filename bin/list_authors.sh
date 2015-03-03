d75b98a19bedb4814d0ecce072fc7038369e17fd
Fabricio C Zuardi <fabricio@fabricio.org>
2015-03-03 11:35:28 -0300

    sort author list by proposal id

--- bin/list_authors.sh
+++ bin/list_authors.sh
@@ -1,1 +1,7 @@
-grep -H 'name\"' data/*|less
+DATAPATH="data/"
+for i in `ls -1v $DATAPATH`;do 
+  LINE=`cat "$DATAPATH$i"|grep "name\""|cut -d":" -f2`
+  if [ ! -z "$LINE" ];then 
+    echo "$i\t$LINE"
+  fi;
+done;
