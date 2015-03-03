07e2d9abf8fd4e9ce1e3f2a009a0097e221fe124
Fabricio C Zuardi <fabricio@fabricio.org>
2015-03-03 11:05:19 -0300

    properly list titles ordered by id

--- bin/list_titles.sh
+++ bin/list_titles.sh
@@ -1,1 +1,12 @@
-grep -H 'title\"' data/*|less
+TEMP_FILE="/tmp/titlelist"
+DATA_FOLDER="./data/"
+OUTPUT=""
+NEW_TITLE=""
+
+for i in `ls -1v data`;do 
+  NEW_TITLE=`cat $DATA_FOLDER$i|grep "title\""|cut -d":" -f2`
+  if [ ! -z "$NEW_TITLE" ]; then
+    OUTPUT="$OUTPUT\n$i:$NEW_TITLE"
+  fi
+done;
+echo $OUTPUT|less
